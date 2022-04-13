b7761250374a0f39d8a6f89bfafaf7428987129a
HaliemGit <test@haliem.com>
2022-04-13 19:06:44 +0200

    Adding def run testcase to me

--- me.py
+++ me.py
@@ -4,6 +4,11 @@ import tkinter as tk
 from tkinter import Tk, ttk
 from tkinter.messagebox import showinfo
 from turtle import onclick
+import subprocess
+import os
+import sys
+from datetime import datetime, timedelta
+from threading import Timer
 #from numpy import var
 #from click import command
 #from setuptools import Command
@@ -14,7 +19,7 @@ class The_App():
         
         self.root = tk.Tk()
         self.root.title('Run Robot Test Cases')
-        self.root.geometry('620x200')
+        self.root.geometry('720x200')
         self.columns = ('first_name')   # define columns
         self.tree = ttk.Treeview(self.root, columns=self.columns, show='headings')
         self.tree.heading('first_name', text='Test Cases')  # define headings
@@ -32,7 +37,10 @@ class The_App():
         for contact in contacts:
             self.tree.insert('', tk.END, values=contact)
 
-
+        def selected_Testcase():
+            curItem = self.tree.focus()
+            #print(self.tree.item(curItem)['values'][0])
+            return self.tree.item(curItem)['values'][0]
 
         def item_selected_(event):
             for item in self.tree.selection():
@@ -46,9 +54,18 @@ class The_App():
         self.tree.bind('<<TreeviewSelect>>', item_selected_)
 
 
+        def Run_test():
+            current_dir = os.path.abspath(__file__)
+            head, tiel = os.path.split(current_dir)
+            test_case = selected_Testcase()
+            Running_test_case = head + "\\"+test_case
+            python_pathy = sys.executable
+            subprocess.call([python_pathy,'-m','robot',Running_test_case])
+            
+            
 
         self.tree.grid(row=0, column=0, sticky='nsew')
-        btn = tk.Button(self.root, text = 'Run!', bd = '1',command = self.root.destroy)
+        btn = tk.Button(self.root, text = 'Run Test case', bd = '1',command =Run_test)
         btn.place(x=625, y=20)
 
 # add a scrollbar
