0ea4ff0fc6612283a0818837647812a225b78ea2
HaliemGit <test@haliem.com>
2022-05-01 16:13:35 +0200

    example to get checkbox value in vari

--- vari.py
+++ vari.py
@@ -6,3 +6,28 @@ class variables():
     pass
 
 #variables.returning_hours()
+from tkinter import Tk, Checkbutton, IntVar, Frame, Label
+from functools import partial
+
+task_list = ['Task 1', 'Task 2', 'Task 3', 'Work', 'Study']
+
+
+def choose(index, task):
+    print(f'Selected task: {task}' if var_list[index].get() == 1 else f'Unselected task: {task}')
+
+
+root = Tk()
+
+Label(root, text='Tasks').grid(column=0, row=0)
+
+frame = Frame(root)
+frame.grid(column=0, row=1)
+
+var_list = []
+
+for index, task in enumerate(task_list):
+    var_list.append(IntVar(value=0))
+    Checkbutton(frame, variable=var_list[index],
+                text=task, command=partial(choose, index, task)).pack()
+
+root.mainloop()
